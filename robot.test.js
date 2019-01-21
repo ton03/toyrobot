4e08f5f0f48e9e5468d1fbd280654db748e77e67
Antonn Esquivel <esquivel.antonn@gmail.com>
2019-01-21 22:57:56 +1100

    a bunch of test cases

--- robot.test.js
+++ robot.test.js
@@ -6,6 +6,9 @@ beforeEach(() => {
   robot = Object.create(Robot);
 });
 
+// Mock console.log to "disable" logging
+jest.spyOn(console, 'log').mockImplementation(() => jest.fn());
+
 describe('Set Robot position and table bounds', () => {
   test('default position at (0, 0)', () => {
     expect(robot.xPos).toBe(0);
@@ -23,19 +26,19 @@ describe('Set Robot position and table bounds', () => {
     expect(robot.dir).toBe('EAST');
   });
 
-  test('set table bounds to 10x10', () => {
+  test('default table bounds (5x5)', () => {
+    expect(robot.xBounds).toBe(5);
+    expect(robot.yBounds).toBe(5);
+  });
+
+  test('set table bounds to 10x20', () => {
     robot.setTableBounds(10, 20);
     expect(robot.xBounds).toBe(10);
     expect(robot.yBounds).toBe(20);
   });
-
-  test('undefined table bounds', () => {
-    expect(robot.xBounds).toBeUndefined();
-    expect(robot.yBounds).toBeUndefined();
-  });
 });
 
-describe('Robot reporting', () => {
+describe('Robot reporting its position', () => {
   let consoleSpy;
 
   beforeEach(() => {
@@ -51,5 +54,184 @@ describe('Robot reporting', () => {
     robot.setPosition(6, 7, 'SOUTH');
     robot.report();
     expect(consoleSpy).toHaveBeenCalledWith('6,7,SOUTH');
+  });
+});
+
+describe('Simple robot movement and turning', () => {
+  test('move north once', () => {
+    robot.setPosition(0, 0, 'NORTH');
+    robot.move();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(1);
+    expect(robot.dir).toBe('NORTH');
+  });
+
+  test('move south once', () => {
+    robot.setPosition(0, 1, 'SOUTH');
+    robot.move();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(0);
+    expect(robot.dir).toBe('SOUTH');
+  });
+
+  test('move west once', () => {
+    robot.setPosition(1, 0, 'WEST');
+    robot.move();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(0);
+    expect(robot.dir).toBe('WEST');
+  });
+
+  test('move east once', () => {
+    robot.setPosition(0, 0, 'EAST');
+    robot.move();
+    expect(robot.xPos).toBe(1);
+    expect(robot.yPos).toBe(0);
+    expect(robot.dir).toBe('EAST');
+  });
+
+  test('facing north, turning right to face east', () => {
+    robot.setPosition(0, 0, 'NORTH');
+    robot.turn('RIGHT');
+    expect(robot.dir).toBe('EAST');
+  });
+
+  test('facing east, turning right to face south', () => {
+    robot.setPosition(0, 0, 'EAST');
+    robot.turn('RIGHT');
+    expect(robot.dir).toBe('SOUTH');
+  });
+
+  test('facing south, turning right to face west', () => {
+    robot.setPosition(0, 0, 'SOUTH');
+    robot.turn('RIGHT');
+    expect(robot.dir).toBe('WEST');
+  });
+
+  test('facing west, turning right to face north', () => {
+    robot.setPosition(0, 0, 'WEST');
+    robot.turn('RIGHT');
+    expect(robot.dir).toBe('NORTH');
+  });
+
+  test('facing north, turning left to face west', () => {
+    robot.setPosition(0, 0, 'NORTH');
+    robot.turn('LEFT');
+    expect(robot.dir).toBe('WEST');
+  });
+
+  test('facing west, turning left to face south', () => {
+    robot.setPosition(0, 0, 'WEST');
+    robot.turn('LEFT');
+    expect(robot.dir).toBe('SOUTH');
+  });
+
+  test('facing south, turning left to face east', () => {
+    robot.setPosition(0, 0, 'SOUTH');
+    robot.turn('LEFT');
+    expect(robot.dir).toBe('EAST');
+  });
+
+  test('facing east, turning left to face north', () => {
+    robot.setPosition(0, 0, 'EAST');
+    robot.turn('LEFT');
+    expect(robot.dir).toBe('NORTH');
+  });
+});
+
+describe('Prevent robot from falling off', () => {
+  test('do not fall off the west side', () => {
+    robot.setPosition(0, 2, 'WEST');
+    robot.move();
+    robot.move();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(2);
+  });
+
+  test('do not fall off the north side', () => {
+    robot.setPosition(2, 4, 'NORTH');
+    robot.move();
+    robot.move();
+    expect(robot.xPos).toBe(2);
+    expect(robot.yPos).toBe(4);
+  });
+
+  test('do not fall off the east side', () => {
+    robot.setPosition(4, 2, 'EAST');
+    robot.move();
+    robot.move();
+    expect(robot.xPos).toBe(4);
+    expect(robot.yPos).toBe(2);
+  });
+
+  test('do not fall off the south side', () => {
+    robot.setPosition(2, 0, 'SOUTH');
+    robot.move();
+    robot.move();
+    expect(robot.xPos).toBe(2);
+    expect(robot.yPos).toBe(0);
+  });
+});
+
+describe('Moving and turning combination', () => {
+  test('move 1 cell north east', () => {
+    robot.move();
+    robot.turn('RIGHT');
+    robot.move();
+    expect(robot.xPos).toBe(1);
+    expect(robot.yPos).toBe(1);
+  });
+
+  test('zigzag from north west corner to south east corner', () => {
+    robot.setPosition(0, 4, 'EAST');
+    robot.move();
+    robot.turn('RIGHT');
+    robot.move();
+    robot.turn('LEFT');
+    robot.move();
+    robot.turn('RIGHT');
+    robot.move();
+    robot.turn('LEFT');
+    robot.move();
+    robot.turn('RIGHT');
+    robot.move();
+    robot.turn('LEFT');
+    robot.move();
+    robot.turn('RIGHT');
+    robot.move();
+    expect(robot.xPos).toBe(4);
+    expect(robot.yPos).toBe(0);
+    expect(robot.dir).toBe('SOUTH');
+  });
+
+  test('continue moving on all directions and hope not to fall off', () => {
+    const charge = () => {
+      for (let i = 0; i < 20; ++i) {
+        robot.move();
+      }
+    };
+
+    // Charge to north west corner
+    charge();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(4);
+
+    // Charge to north east corner
+    robot.turn('RIGHT');
+    charge();
+    expect(robot.xPos).toBe(4);
+    expect(robot.yPos).toBe(4);
+
+    // Charge to south east corner
+    robot.turn('RIGHT');
+    charge();
+    expect(robot.xPos).toBe(4);
+    expect(robot.yPos).toBe(0);
+
+    // Charge to south west corner
+    robot.turn('RIGHT');
+    charge();
+    expect(robot.xPos).toBe(0);
+    expect(robot.yPos).toBe(0);
   });
 });
